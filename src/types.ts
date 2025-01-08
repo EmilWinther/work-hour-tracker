24d2afe2b2a8546111cacc8b86cf14afaf99afb3
Emil Winther <ekmul123@gmail.com>
2025-01-08 19:15:28 +0100

    Init commit

---
+++ src/types.ts
@@ -0,0 +1,9 @@
+export interface WorkWeek {
+    weekNumber: number;
+    monday: number | null;
+    tuesday: number | null;
+    wednesday: number | null;
+    thursday: number | null;
+    friday: number | null;
+    lastUpdated: string;
+}
